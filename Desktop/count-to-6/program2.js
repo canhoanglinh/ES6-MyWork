46dc5cc36445dc10ce8f054d778b3960d9f1b7e6
Can Hoang Linh <canhoanglinh98@gmail.com>
2019-06-13 10:28:00 +0700

    first commit

---
+++ Desktop/count-to-6/program2.js
@@ -0,0 +1,6 @@
+let intro = "Hello,";
+let name= (process.argv[2]);
+let full = intro + ' ' + name + '!';
+console.log(full);
+console.log("Your name lowercased is " + "\""+  name.toLowerCase() + "\"." );
+
